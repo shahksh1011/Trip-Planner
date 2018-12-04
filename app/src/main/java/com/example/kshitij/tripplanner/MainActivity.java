3423d80a28777bdfefbd7411cf0d9b8f5bf21e30
kshitijshah <shahksh1011@gmail.com>
2018-12-04 14:48:32 -0500

    second commit

--- app/src/main/java/com/example/kshitij/tripplanner/MainActivity.java
+++ app/src/main/java/com/example/kshitij/tripplanner/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.kshitij.tripplanner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+//    private GoogleSignInClient mGoogleSignInClient;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+    }
+    private void signIn() {
+        Intent i = new Intent(getApplicationContext(), GoogleSignInActivity.class);
+        startActivity(i);
+//        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+//        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser == null){
+            signIn();
+        }
+        else {
+            Toast.makeText(getApplicationContext(), "Welcome Sir",Toast.LENGTH_LONG).show();
+        }
     }
 }
---
+++ app/src/main/java/com/example/kshitij/tripplanner/TripListActivity.java
@@ -0,0 +1,14 @@
+
+package com.example.kshitij.tripplanner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TripListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip_list);
+    }
+}
