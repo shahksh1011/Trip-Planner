3d6c1adaa6e9c298b1be4dc04bbb7bd9e098e1de
kshitijshah <shahksh1011@gmail.com>
2018-12-04 15:18:34 -0500

    updated code

---
+++ app/src/main/java/com/example/kshitij/tripplanner/Trip.java
@@ -0,0 +1,5 @@
+package com.example.kshitij.tripplanner;
+
+public class Trip {
+    String tripName;
+}
--- app/src/main/java/com/example/kshitij/tripplanner/TripListActivity.java
+++ app/src/main/java/com/example/kshitij/tripplanner/TripListActivity.java
@@ -1,8 +1,12 @@
 
 package com.example.kshitij.tripplanner;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class TripListActivity extends AppCompatActivity {
 
@@ -10,5 +14,30 @@ public class TripListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip_list);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.trip_list_menu_activity, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.addTrip:
+                Intent i = new Intent(getApplicationContext(), AddTripActivity.class);
+                startActivity(i);
+                return true;
+
+            case R.id.logOutButton:
+                finish();
+                return true;
+             default:
+                 return super.onOptionsItemSelected(item);
+        }
+
+
     }
 }
